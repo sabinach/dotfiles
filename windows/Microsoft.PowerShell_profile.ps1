ffe3240d9dcc5e6cb55aaa49c918520887cb749e
Sabina Chen <15172577+sabinach@users.noreply.github.com>
2023-05-24 15:35:53 -0400

    Update Microsoft.PowerShell_profile.ps1

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-# Filepath: C:\Users\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# Filepath: C:\Users\SabinaChen\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
 function goToDesktop {
     cd "C:\Users\SabinaChen\Desktop"
